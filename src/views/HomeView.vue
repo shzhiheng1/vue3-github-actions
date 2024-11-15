0e3e6cf510e74c8397a039d8bd62588b1d06ac5d
shizhiheng <2225018807@qq.com>
2024-11-15 10:15:55 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>测试gitHub自动化部署功能</h1>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </main>
+</template>
