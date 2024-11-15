f86b98f1c84d31a439998fe6d7c431c21e20cfdc
shzhiheng1 <shzhiheng1@users.noreply.github.com>
2024-11-15 08:17:20 +0000

    deploy: db64fd4f67756452d1daeaa161150d8acfd97e2d

---
+++ assets/AboutView-Dv8VrlK8.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,a as s,o as a}from"./index-DiefOsrb.js";const c={},n={class:"about"};function r(_,e){return a(),t("div",n,e[0]||(e[0]=[s("h1",null,"关于的页面",-1)]))}const d=o(c,[["render",r]]);export{d as default};
