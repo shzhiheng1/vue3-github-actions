81cb45b3c54e77d14a198a3c5e4a97d7665c462c
shizhiheng <2225018807@qq.com>
2024-11-15 16:14:48 +0800

    aaa

--- vite.config.js
+++ vite.config.js
@@ -16,25 +16,25 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  build: {
-    target: 'esnext', // 支持的JavaScript版本
-    outDir: 'dist', // 构建时输出目录
-    assetsDir: 'assets', // 放置生成的静态资源的目录
-    minify: 'terser', // 压缩方式
-    terserOptions: {
-      compress: {
-        drop_console: true, // 是否删除所有的console语句
-      },
-    },
-    // 是否为生产环境构建生成source map
-    sourcemap: false,
-    // 构建后是否进行代码分割
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`,
-      },
-    },
-  }
+  // build: {
+  //   target: 'esnext', // 支持的JavaScript版本
+  //   outDir: 'dist', // 构建时输出目录
+  //   assetsDir: 'assets', // 放置生成的静态资源的目录
+  //   minify: 'terser', // 压缩方式
+  //   terserOptions: {
+  //     compress: {
+  //       drop_console: true, // 是否删除所有的console语句
+  //     },
+  //   },
+  //   // 是否为生产环境构建生成source map
+  //   sourcemap: false,
+  //   // 构建后是否进行代码分割
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: `assets/[name].[hash].js`,
+  //       chunkFileNames: `assets/[name].[hash].js`,
+  //       assetFileNames: `assets/[name].[hash].[ext]`,
+  //     },
+  //   },
+  // }
 })
